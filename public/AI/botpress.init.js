1fd56f4f24ac81cb0e212246793e6b69bf6bab69
anku0910 <20040910a@gmail.com>
2025-05-23 18:06:32 +0800

    AI跟前端

---
+++ public/AI/botpress.init.js
@@ -0,0 +1,20 @@
+window.botpress.init({
+  "botId": "8db1604b-6b48-4919-9a5c-4b3eb23948a4",
+  "configuration": {
+    "botName": "AI小老師",
+    "botAvatar": "https://files.bpcontent.cloud/2025/05/20/06/20250520062614-9U6T9BFK.png",
+    "fabImage": "https://files.bpcontent.cloud/2025/05/20/06/20250520062438-1O49HB49.png",
+    "website": {},
+    "email": {},
+    "phone": {},
+    "termsOfService": {},
+    "privacyPolicy": {},
+    "color": "#b75e14",
+    "variant": "soft",
+    "themeMode": "light",
+    "fontFamily": "ibm",
+    "radius": 3,
+    "additionalStylesheetUrl": "https://files.bpcontent.cloud/2025/05/20/06/20250520062811-KX8CLOG6.css"
+  },
+  "clientId": "51db9fc7-e92e-41f3-a0e2-d4105907deab"
+});
